16eecc690f44d6798822654a307cbf08074a37e8
estanislauaaraojunior <estanislau.aarao@gmail.com>
2026-01-16 14:09:32 -0300

    Adicionar arquivo aula_1.js com variáveis e exibição no console

---
+++ Aulas_de_JS/aula_1.js
@@ -0,0 +1,19 @@
+/*Quero que você escreva três variáveis:
+
+seu nome
+
+sua idade
+
+uma lista com três coisas que você gosta
+
+Use const para o que não muda e let para o que pode mudar.*/
+
+
+const nome = "Estanislau"; // Declaração de uma variável string
+let idade = 42; // Declaração de uma variável numérica
+const hobbies = ["Estudar", "Viajar", "Praia"]; // Declaração de um array com três elementos
+
+console.log("Olá, meu nome é " + nome + ".")
+console.log(" Eu tenho " + idade + " anos.")
+console.log("Gosto de " + hobbies[0] + ", " + hobbies[1] + " e " + hobbies[2] + ".")
+// Exibindo as variáveis no console
