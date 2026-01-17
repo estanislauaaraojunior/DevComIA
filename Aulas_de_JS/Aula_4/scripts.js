e2657bf58e1ad7c982833605053eea2a555af8e8
estanislauaaraojunior <estanislau.aarao@gmail.com>
2026-01-17 14:54:05 -0300

    Adicionar validações e limpeza de entrada na função analisar, além de criar novos arquivos HTML e CSS para a Aula 4.2

--- Aulas_de_JS/Aula_4/scripts.js
+++ Aulas_de_JS/Aula_4/scripts.js
@@ -19,9 +19,43 @@ function descrevePessoa(nome, idade, hobbies) {
 }
 
 function analisar() {
-    const nome = document.getElementById("nomeInput").value;            /*pega o valor do input*/
+    const nome = document.getElementById("nomeInput").value.trim();            /*pega o valor do input*/
     const idade = Number(document.getElementById("idadeInput").value);      /*pega o valor do input e converte para número*/
-    const hobbies = document.getElementById("hobbieInput").value.split(",");        /*pega o valor do input e cria um array separando pelos vírgulas*/
+    const hobbiesTexto = document.getElementById("hobbieInput").value.trim();        /*pega o valor do input e cria um array separando pelos vírgulas*/
+
+    // Validação simples para nome vazio
+    if (nome === "") {
+        document.getElementById("resultado").textContent = "Por favor, insira um nome válido.";
+        return;
+    }
+
+    // Validação simples para idade negativa ou não numérica
+    if (!idade || idade < 0) {
+        document.getElementById("resultado").textContent = "Por favor insira uma idade válida.";
+        return;
+    }
+
+    // Validação simples para hobbies vazios
+    if (hobbiesTexto === "") {
+        document.getElementById("resultado").textContent = "Por favor, insira pelo menos um hobbie.";
+        return;
+    }
+
+    //Transformar hobbies em array limpo (sem espaços extras)
+    const hobbies = hobbiesTexto        /*pega o texto dos hobbies*/
+    .split (",")               /*separa os hobbies em um array*/
+    .map (h => h.trim())      /*remove espaços extras*/
+    .filter (h => h !== "");          /*remove hobbies vazios*/
+
+    if (hobbies.length === 0) {
+        document.getElementById("resultado").textContent = "Digite um hobbie válido, separados por virgulas.";
+        return;
+    }
+
     const texto = descrevePessoa(nome, idade, hobbies);             /*chama a função descrevePessoa e armazena o resultado na variável texto*/
     document.getElementById("resultado").textContent = texto;       /*exibe o resultado na página*/
 }
+
+
+
+
