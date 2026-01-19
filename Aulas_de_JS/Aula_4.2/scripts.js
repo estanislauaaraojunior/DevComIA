b5c7ca9addaec5daee9d9511c002dd9cfd2b1b5c
estanislauaaraojunior <estanislau.aarao@gmail.com>
2026-01-19 11:20:47 -0300

    Adicionar lógica de cálculo do IMC e mensagens de classificação, além de criar um novo arquivo HTML para testes

--- Aulas_de_JS/Aula_4.2/scripts.js
+++ Aulas_de_JS/Aula_4.2/scripts.js
@@ -1,6 +1,5 @@
 
 
-
 function calcularIMC() {
     const peso = Number(document.getElementById("pesoInput").value);
     const altura = Number(document.getElementById("alturaInput").value);
@@ -9,3 +8,5 @@ function calcularIMC() {
  document.getElementById("resultado").textContent = "Seu indice de massa corporal é: " + resultado.toFixed(2);
 }
 
+console.log("Script carregado com sucesso!");
+console.log(calcularIMC());
